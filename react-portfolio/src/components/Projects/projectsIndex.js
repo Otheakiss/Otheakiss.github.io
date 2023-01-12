cf96fa9143ab795e125f0b544de20c4bbbcc5d3e
Brittany Weaver <brittanyweaver13@gmail.com>
2023-01-12 09:45:00 -0500

    created projects page

---
+++ react-portfolio/src/components/Projects/projectsIndex.js
@@ -0,0 +1,45 @@
+import './projectsIndex.scss';
+import Loaders from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters/animatedLettersIndex';
+import { useEffect, useState } from 'react';
+import tastebuds from '../../assets/images/tastebuds.png'
+
+const Projects = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+      
+    useEffect(() => {
+      let timeout;
+  
+      timeout = setTimeout(() => {
+        setLetterClass("text-animate-hover");
+      }, 3000);
+  
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, []);
+
+    return (
+        <>
+        <div className='project-page'>
+            <div className='text-zone'>
+            <h1>
+                <AnimatedLetters letterClass={letterClass} strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+                idx={15} />
+            </h1>
+            <h3>tastebuds</h3>
+            <p>Designed to find the perfect place to dine. Restaurant matching and voting application using MVC pattern and RESTful APIs. 
+                Registered Taste Buds users can search for restaurants in a desired area and create scheduled events. 
+                Friends are invited via unique links to view restaurant information and submit votes. The program will record votes and calculate the winning restaurants.
+            </p>
+            </div>
+        </div>
+        <div className='projects-picture'>
+            <img src={tastebuds} alt='tastebuds'></img>
+            
+        </div>
+        <Loaders type='pacman' />
+        </>
+    )
+}
+export default Projects
