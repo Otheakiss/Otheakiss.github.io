347309c0647567efe6432f8cebc7b309ccfa0462
Brittany Weaver <brittanyweaver13@gmail.com>
2023-02-04 17:35:08 -0500

    fixed sidebar mobile view

--- react-portfolio/src/components/Projects/projectsIndex.js
+++ react-portfolio/src/components/Projects/projectsIndex.js
@@ -5,38 +5,39 @@ import { useEffect, useState } from 'react';
 import tastebuds from '../../assets/images/tastebuds.png'
 
 const Projects = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-      
-    useEffect(() => {
-      let timeout;
-  
-      timeout = setTimeout(() => {
-        setLetterClass("text-animate-hover");
-      }, 3000);
-  
-      return () => {
-        clearTimeout(timeout);
-      };
-    }, []);
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    let timeout;
+
+    timeout = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
 
     return (
         <>
-        <div className='container project-page'>
+        <div className="container project-page">
             <div className='text-zone'>
             <h1>
-                <AnimatedLetters letterClass={letterClass} strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', ' ', '#', '1']}
-                idx={15} />
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+                idx={15}
+                 />
             </h1>
             <h3>tastebuds</h3>
             <p>Designed to find the perfect place to dine. Restaurant matching and voting application using MVC pattern and RESTful APIs. 
-                Registered Taste Buds users can search for restaurants in a desired area and create scheduled events. 
-                Friends are invited via unique links to view restaurant information and submit votes. The program will record votes and calculate the winning restaurants.
+               Registered Taste Buds users can search for restaurants in a desired area and create scheduled events. 
+               Friends are invited via unique links to view restaurant information and submit votes. The program will record votes and calculate the winning restaurants.
             </p>
             </div>
         </div>
         <div className='container projects-picture'>
             <img src={tastebuds} alt='tastebuds' className=''></img>
-            
         </div>
         <Loaders type='pacman' />
         </>
--- react-portfolio/src/components/Sidebar/sidebarIndex.js
+++ react-portfolio/src/components/Sidebar/sidebarIndex.js
@@ -1,14 +1,16 @@
 import './sidebarIndex.scss';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import LogoW from '../../assets/images/LogoW.png';
 import LogoSubtitle from '../../assets/images/LogoSubtitle.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faSuitcase, faUser, faBars, faPenFancy } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Sidebar = () => {
-    
+    const [showNav, setShowNav] = useState(false);
+
     return (
         <>
     <div className='nav-bar'>
@@ -16,18 +18,21 @@ const Sidebar = () => {
             <img src={LogoW} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="Weaver" />
         </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/" >
+        <nav className={showNav ? 'mobile-show' : ''}>
+            <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faHome} />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faUser} />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects" >
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faSuitcase} />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
                 <FontAwesomeIcon icon={faEnvelope} />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills" onClick={() => setShowNav(false)}>
+                <FontAwesomeIcon icon={faPenFancy} />
             </NavLink>
             <a href='https://www.linkedin.com/in/brittanyweaver13' target="_blank" rel='noreferrer' className="linkedin-link">
                 <FontAwesomeIcon icon={faLinkedin} />
@@ -36,6 +41,12 @@ const Sidebar = () => {
             <FontAwesomeIcon icon={faGithub} />
             </a>
         </nav>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#e25b45"
+          size="3x"
+          className='hamburger-icon' />
     </div>
     </>
     )
